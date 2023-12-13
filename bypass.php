e295154ca000a5df20bd70cda13c0910181d1d5b
ThisJackRN <jackakel06@gmail.com>
2023-12-13 18:00:13 +0000

    One Bypasses added so far uh oh stinky

--- bypass.php
+++ bypass.php
@@ -6,22 +6,45 @@ include('navbar.php');
 // Specify the folder path to check for webpages
 $folderPath = './bypass/';
 
-// Get all files in the folder with a .html extension
-$files = glob($folderPath . '*.html');
+// Function to get all HTML files in a folder and its subdirectories
+function getHtmlFiles($folder) {
+    $files = glob($folder . '*.html');
+    foreach (glob($folder . '*', GLOB_ONLYDIR) as $subfolder) {
+        $files = array_merge($files, getHtmlFiles($subfolder . '/'));
+    }
+    return $files;
+}
+
+// Get all HTML files in the specified folder and its subdirectories
+$files = getHtmlFiles($folderPath);
 
 // Check if any files are found
 if (count($files) > 0) {
-    // Sort the files alphabetically
-    sort($files);
-
-    // Output an ordered list of webpages
-    echo '<h2>Webpages:</h2>';
-    echo '<ol>';
+    // Organize files by folder
+    $organizedFiles = [];
     foreach ($files as $file) {
+        $folder = dirname($file);
         $fileName = basename($file);
-        echo '<li><a href="' . $file . '">' . $fileName . '</a></li>';
+        $organizedFiles[$folder][] = $fileName;
     }
-    echo '</ol>';
+
+    // Output the organized list
+    echo '<h2>Webpages:</h2>';
+    echo '<div style="text-align: center;">'; // Center the list
+    foreach ($organizedFiles as $folder => $files) {
+        // Remove index.html from under the link
+        $folderLink = rtrim($folder, '/');
+        echo '<h3><a href="' . $folderLink . '/">' . basename($folder) . '</a></h3>';
+        echo '<ul>';
+        foreach ($files as $file) {
+            // Skip index.html in the list
+            if ($file != 'index.html') {
+                echo '<li><a href="' . $folderLink . '/' . $file . '">' . $file . '</a></li>';
+            }
+        }
+        echo '</ul>';
+    }
+    echo '</div>';
 } else {
     // Output a message if no webpages are found
     echo '<p>No bypasses :\'(</p>';
