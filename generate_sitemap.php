b631d2f9517e9513235a611eb5b018443730b5de
ThisJackRN <jackakel06@gmail.com>
2023-12-23 18:32:31 -0500

    included php and html

--- generate_sitemap.php
+++ generate_sitemap.php
@@ -4,9 +4,9 @@ ini_set('display_errors', 0);
 
 function generateSitemap($directory) {
     $lastSitemapUpdate = file_exists('sitemap.xml') ? filemtime('sitemap.xml') : 0;
-    $latestHtmlFileUpdate = getLatestHtmlFileUpdate($directory);
+    $latestFileUpdate = getLatestFileUpdate($directory);
 
-    if ($latestHtmlFileUpdate > $lastSitemapUpdate) {
+    if ($latestFileUpdate > $lastSitemapUpdate) {
         updateSitemap($directory);
     }
 }
@@ -15,16 +15,15 @@ function updateSitemap($directory) {
     $sitemap = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
     $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 
-    $htmlFiles = findHtmlFiles($directory);
+    $files = findFiles($directory, ['html', 'php']);
 
-    if ($htmlFiles === false) {
-        error_log('Error reading HTML files.');
+    if ($files === false) {
+        error_log('Error reading files.');
         return;
     }
 
-    foreach ($htmlFiles as $htmlFile) {
-        $relativePath = getRelativePath($directory, $htmlFile);
-        $relativePath = str_replace('\\', '/', $relativePath); // Replace backslashes with forward slashes
+    foreach ($files as $file) {
+        $relativePath = getRelativePath($directory, $file);
         $sitemap .= '<url>' . PHP_EOL;
         $sitemap .= '  <loc>https://jacksdamnserver.site' . $relativePath . '</loc>' . PHP_EOL;
         $sitemap .= '</url>' . PHP_EOL;
@@ -39,8 +38,8 @@ function updateSitemap($directory) {
     }
 }
 
-function findHtmlFiles($directory) {
-    $htmlFiles = [];
+function findFiles($directory, $extensions) {
+    $files = [];
     $iterator = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS),
         RecursiveIteratorIterator::SELF_FIRST,
@@ -48,24 +47,24 @@ function findHtmlFiles($directory) {
     );
 
     foreach ($iterator as $file) {
-        if ($file->isFile() && $file->getExtension() === 'html') {
-            $htmlFiles[] = $file->getPathname();
+        if ($file->isFile() && in_array($file->getExtension(), $extensions)) {
+            $files[] = $file->getPathname();
         }
     }
 
-    return $htmlFiles;
+    return $files;
 }
 
 function getRelativePath($directory, $file) {
     return '/' . ltrim(str_replace(DIRECTORY_SEPARATOR, '/', substr($file, strlen($directory))), '/');
 }
 
-function getLatestHtmlFileUpdate($directory) {
+function getLatestFileUpdate($directory) {
     $latestUpdate = 0;
-    $htmlFiles = findHtmlFiles($directory);
+    $files = findFiles($directory, ['html', 'php']);
 
-    foreach ($htmlFiles as $htmlFile) {
-        $fileUpdate = filemtime($htmlFile);
+    foreach ($files as $file) {
+        $fileUpdate = filemtime($file);
         $latestUpdate = max($latestUpdate, $fileUpdate);
     }
 
