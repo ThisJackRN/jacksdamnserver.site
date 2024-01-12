daf3a17b24ac8e71acb674f6bc38a5a44eecac50
ThisJackRN <jackakel06@gmail.com>
2024-01-12 01:02:17 -0500

    w

--- admin_panel.php
+++ admin_panel.php
@@ -7,7 +7,7 @@ session_start();
 // Check if the user is not logged in
 if (!isset($_SESSION['user_id'])) {
     // Redirect to the login page or perform any other action
-    header("Location: login.php");
+    header("Location: admin_panel.php");
     exit();
 }
 
